d4a2a64bc04697221ecb5bd4d87468018b8a47f0
Serhii Sazonko <ssazotc@training.local>
2018-11-29 11:47:17 +0200

    to heroku 5

--- app-server/dist/main.js
+++ app-server/dist/main.js
@@ -14113,7 +14113,7 @@ window.global = window;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Dp-142\29.11.18\syncforms\app-client\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! D:\heroku\sync-forms\app-client\src\main.ts */"./src/main.ts");
 
 
 /***/ }),
